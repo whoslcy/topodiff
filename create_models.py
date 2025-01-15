9a54910ade984311eff59fcfa0fb1b7553d44c60
Charles 李辰宇 <shawnaigame@qq.com>
2025-01-15 17:33:48 +0800

    refactor

--- create_models.py
+++ create_models.py
@@ -5,7 +5,7 @@ from topodiff.unet import (
     EncoderUNetModel,
 )
 from topodiff import gaussian_diffusion as gd
-from topodiff.respace import SpacedDiffusion
+from topodiff.respace import SpacedDiffusion, space_timesteps
 from typing import List
 import math
 import numpy as np
@@ -71,51 +71,6 @@ for i in range(steps):
     t2 = (i + 1) / steps
     betas.append(min(1 - alpha_bar(t2) / alpha_bar(t1), 0.999))
 betas = np.array(betas)
-
-def space_timesteps(num_timesteps, section_counts):
-    """
-    Create a list of timesteps to use from an original diffusion process,
-    given the number of timesteps we want to take from equally-sized portions
-    of the original process.
-
-    For example, if there's 300 timesteps and the section counts are [10,15,20]
-    then the first 100 timesteps are strided to be 10 timesteps, the second 100
-    are strided to be 15 timesteps, and the final 100 are strided to be 20.
-
-    If the stride is a string starting with "ddim", then the fixed striding
-    from the DDIM paper is used, and only one section is allowed.
-
-    :param num_timesteps: the number of diffusion steps in the original
-                          process to divide up.
-    :param section_counts: either a list of numbers, or a string containing
-                           comma-separated numbers, indicating the step count
-                           per section. As a special case, use "ddimN" where N
-                           is a number of steps to use the striding from the
-                           DDIM paper.
-    :return: a set of diffusion steps from the original process to use.
-    """
-    size_per = num_timesteps // len(section_counts)
-    extra = num_timesteps % len(section_counts)
-    start_idx = 0
-    all_steps = []
-    for i, section_count in enumerate(section_counts):
-        size = size_per + (1 if i < extra else 0)
-        if size < section_count:
-            raise ValueError(
-                f"cannot divide section of {size} steps into {section_count}"
-            )
-        if section_count <= 1:
-            frac_stride = 1
-        else:
-            frac_stride = (size - 1) / (section_count - 1)
-        cur_idx = 0.0
-        taken_steps = []
-        for _ in range(section_count):
-            taken_steps.append(start_idx + round(cur_idx))
-            cur_idx += frac_stride
-        all_steps += taken_steps
-        start_idx += size
-    return set(all_steps)
 
 def spaced_diffusion():
     return SpacedDiffusion(
