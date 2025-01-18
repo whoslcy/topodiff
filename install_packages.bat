1246058438bed4f486abeadfbc601547926e5b66
Charles 李辰宇 <shawnaigame@qq.com>
2025-01-19 01:23:21 +0800

    update

--- install_packages.bat
+++ install_packages.bat
@@ -1,5 +1,5 @@
 conda install -y^
-  python=3.12 pytorch torchvision torchaudio pytorch-cuda=12.4 mpi4py jupyter blobfile tqdm matplotlib scikit-learn^
+  python=3.12 ipykernel pytorch torchvision torchaudio pytorch-cuda=12.4 mpi4py jupyter blobfile tqdm matplotlib scikit-learn^
   -c pytorch -c nvidia -c conda-forge
 
 REM `solidspy` is not supported on Windows.
